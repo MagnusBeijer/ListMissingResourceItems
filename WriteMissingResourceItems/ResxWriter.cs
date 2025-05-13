315f7f94911cf370e012980a1908489eefd18712
Magnus Persson <magnus.persson@beijerelectronics.com>
2025-05-13 13:38:41 +0200

    Refactor

--- WriteMissingResourceItems/ResxWriter.cs
+++ WriteMissingResourceItems/ResxWriter.cs
@@ -11,6 +11,7 @@ public class ResxWriter
         var fileName = Path.GetFileNameWithoutExtension(mainRexFile);
         var searchPattern = fileName + ".*.resx";
         var langFiles = Directory.EnumerateFiles(path, searchPattern).ToList();
+        var mainKeys = await GetMainKeys(mainRexFile);
 
         foreach (var file in langFiles)
         {
@@ -19,18 +20,30 @@ public class ResxWriter
             if (data.TryGetValue(culture, out var translations))
             {
                 Console.WriteLine("Updating " + file);
-                await WriteResxAsync(file, translations);
+                await WriteResxAsync(file, translations, mainKeys);
             }
         }
     }
 
-    private static async Task WriteResxAsync(string filePath, Dictionary<string, string> values)
+    private static async Task<HashSet<string>> GetMainKeys(string mainRexFile)
+    {
+        await using var stream = File.OpenRead(mainRexFile);
+        return (await XDocument.LoadAsync(stream, LoadOptions.None, CancellationToken.None))
+                    .Element("root")!
+                    .Elements("data")
+                    .Select(e => e.Attribute("name")?.Value)
+                    .Where(key => key != null)
+                    .Select(key => key!)
+                    .ToHashSet(StringComparer.OrdinalIgnoreCase);
+    }
+
+    private static async Task WriteResxAsync(string filePath, Dictionary<string, string> values, HashSet<string> mainKeys)
     {
         XDocument doc;
         await using (var stream = File.OpenRead(filePath))
             doc = await XDocument.LoadAsync(stream, LoadOptions.None, CancellationToken.None);
 
-        XElement? root = doc.Element("root");
+        var root = doc.Element("root");
 
         if (root == null)
             return;
@@ -38,8 +51,25 @@ public class ResxWriter
         var existingElements = root
                                 .Elements("data").Select(e => (Name: e.Attribute("name")?.Value, Value: e.Element("value")))
                                 .Where(x => x.Name != null && x.Value != null)
-                                .ToDictionary(x => x.Name!, x => x.Value!);
+                                .ToDictionary(x => x.Name!, x => x.Value!, StringComparer.OrdinalIgnoreCase);
 
+        AddOrUpdate(values, root, existingElements);
+        Remove(mainKeys, existingElements);
+
+        await using (var stream = File.Create(filePath))
+            await doc.SaveAsync(stream, SaveOptions.None, CancellationToken.None);
+    }
+
+    private static void Remove(HashSet<string> mainKeys, Dictionary<string, XElement> existingElements)
+    {
+        foreach (var item in existingElements.Where(x => !mainKeys.Contains(x.Key)))
+        {
+            item.Value.Parent!.Remove();
+        }
+    }
+
+    private static void AddOrUpdate(Dictionary<string, string> values, XElement root, Dictionary<string, XElement> existingElements)
+    {
         foreach (var entry in values)
         {
             if (existingElements.TryGetValue(entry.Key, out var valueElement))
@@ -54,8 +84,5 @@ public class ResxWriter
                                       new XElement("value", entry.Value)));
             }
         }
-
-        await using (var stream = File.Create(filePath))
-            await doc.SaveAsync(stream, SaveOptions.None, CancellationToken.None);
     }
 }
