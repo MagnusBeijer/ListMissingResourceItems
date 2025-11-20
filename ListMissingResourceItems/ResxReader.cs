7b9db7aaf636190983d1ce57bcd7748342093247
Magnus Persson <magnus.persson@beijerelectronics.com>
2025-11-20 10:53:09 +0100

    #18 Handle the case when no remote file exists. The file was created in the urrent branch.

--- ListMissingResourceItems/ResxReader.cs
+++ ListMissingResourceItems/ResxReader.cs
@@ -13,23 +13,28 @@ public class ResxReader
     public async IAsyncEnumerable<(string key, string? value)> ReadResxFileAsync(TextReader textReader)
     {
         using (textReader)
-        using (var xmlReader = XmlReader.Create(textReader, new XmlReaderSettings { Async = true, }))
         {
-            while (await xmlReader.ReadAsync())
+            if (textReader.Peek() == -1)
+                yield break;
+
+            using (var xmlReader = XmlReader.Create(textReader, new XmlReaderSettings { Async = true, }))
             {
-                if (xmlReader.NodeType == XmlNodeType.Element && xmlReader.Name == "data")
+                while (await xmlReader.ReadAsync())
                 {
-                    string? key = xmlReader.GetAttribute("name");
-
-                    if (key != null)
+                    if (xmlReader.NodeType == XmlNodeType.Element && xmlReader.Name == "data")
                     {
-                        string? value = null;
-                        if (xmlReader.ReadToDescendant("value"))
-                        {
-                            value = await xmlReader.ReadElementContentAsStringAsync();
-                        }
+                        string? key = xmlReader.GetAttribute("name");
 
-                        yield return (key, value);
+                        if (key != null)
+                        {
+                            string? value = null;
+                            if (xmlReader.ReadToDescendant("value"))
+                            {
+                                value = await xmlReader.ReadElementContentAsStringAsync();
+                            }
+
+                            yield return (key, value);
+                        }
                     }
                 }
             }
